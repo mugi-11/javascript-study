228f3ba9b6de7ac8198bdf359e969b6fc15e6a2e
間根山麦 <maneyamamugi@maneyamamuginoMacBook-Air.local>
2022-03-01 21:02:54 +0900

    janken2

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力してください');
+  user_hand = prompt('じゃんけんの手をグー・チョキ・パーから選んでください。');
+}
 
 let js_hand = getJShand();
 
 let judge = winLose(user_hand, js_hand);
 
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+}  else {
+  alert("またチャレンジしてね")
+}
 
 function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
-  let hand_name;
+  let hand;
 
   if(js_hand_num == 0){
-    hand_name = "グー";
+    hand = "グー";
   } else if(js_hand_num == 1){
-    hand_name = "チョキ";
+    hand = "チョキ";
   } else if(js_hand_num == 2){
-    hand_name = "パー";
+    hand = "パー";
   }
 
-  return hand_name;
+  return hand;
 }
 
 function winLose(user, js){
